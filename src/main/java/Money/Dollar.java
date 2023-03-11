4aa203058c1532c7af667c6d3ff6f53b184affe6
pderer <capospi9903@naver.com>
2023-03-12 02:02:41 +0900

    [6장][Refactor] equal() 코드를 위로 올림

--- src/main/java/Money/Dollar.java
+++ src/main/java/Money/Dollar.java
@@ -8,9 +8,4 @@ class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
 }
