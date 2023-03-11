6b9618ffec08bc53ae055be606c07d28f419f240
pderer <capospi9903@naver.com>
2023-03-12 02:53:21 +0900

    [9장][Green] 일단 통과

--- src/main/java/Money/Money.java
+++ src/main/java/Money/Money.java
@@ -5,6 +5,8 @@ abstract class Money {
 
     abstract Money times(int multiplier);
 
+    abstract String currency();
+
     static Money dollar(int amount) {
         return new Dollar(amount);
     }
