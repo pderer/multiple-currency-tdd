18879d75df69915aa9be08865aa1a0668aa740fc
pderer <capospi9903@naver.com>
2023-03-14 14:10:45 +0900

    [14장][Red]
    testArrayEquals : 귀찮은 2가 아직도 테스트와 코드 두 부분에 나오는 것을 없애기 위해 Bank에서 환율표를 가지고 있다가 필요할 때 찾아볼 수 있도록 하자. 두 개의 통화와 환율을 매핑시키는 해시 테이블을 사용하자 -> 통화 쌍을 해시 테이블의 키로 쓰기 위해 배열을 사용할 수 있을까? -> 같은 배열이 equals를 만족하는지 알아본다 -> 테스트 실패 -> 키를 위한 객체를 따로 만들자 -> Pair를 키로 쓰기 위해 equals, hashCode를 구현 -> 지금은 리팩토링 중이기 때문에 태스트를 따로 작성하지 않음 -> 이 리팩토링을 마치고 테스트를 통과한다면, 그때 우리는 그 코드가 실제로 사용되었다고 생각할 수 있다
    testIdentityRate : 테스트를 진행했더니 testReduceMoney에서 빨간 막대 발견  -> USD에서 USD로의 환율을 요청하면, 그 값이 1이 되어야 한다는 것을 알게 됨 -> 나중에 코드를 읽어볼 사람들을 위해 테스트로 작성

--- src/main/java/Money/Bank.java
+++ src/main/java/Money/Bank.java
@@ -1,17 +1,19 @@
 package Money;
 
+import java.util.Hashtable;
+
 class Bank {
+    private Hashtable rates = new Hashtable();
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
-    void addRate(String to, String from, int rate) {
-
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), Integer.valueOf(rate));
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD"))
-                ? 2
-                : 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 }
