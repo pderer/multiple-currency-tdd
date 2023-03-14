3edc55b24691e29e0166110601b6c45b1346caef
pderer <capospi9903@naver.com>
2023-03-14 13:31:31 +0900

    [14장][Refactor]
    Money가 환율을 알게 돼 버림 -> 환율은 모두 Bank가 처리해야 함

--- src/main/java/Money/Bank.java
+++ src/main/java/Money/Bank.java
@@ -2,6 +2,11 @@ package Money;
 
 class Bank {
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
+
+    void addRate(String to, String from, int rate) {
+
+    }
+
 }
--- src/main/java/Money/Expression.java
+++ src/main/java/Money/Expression.java
@@ -1,5 +1,5 @@
 package Money;
 
 interface Expression {
-    Money reduce(String to);
+    Money reduce(Bank bank, String to);
 }
